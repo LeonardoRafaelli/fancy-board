9675343a69207573d21bbce3e55bb11bc8861799
camillyPessotti <leonardorafaelli@gmail.com>
2022-05-13 13:48:29 -0300

    sliding controle updated

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,29 @@ function App() {
 
     const targetValue = parseInt(e.target.value);
     if(e.target.value !== ''){
-      if(e.target.className === "top-left"){
+      if(e.target.className === "top"){
         setTopLeft(targetValue);
       }
-      if(e.target.className === "top-right"){
+      if(e.target.className === "right"){
         setTopRight(targetValue);
       }
-      if(e.target.className === "bottom-right"){
+      if(e.target.className === "bottom"){
         setBottomRight(targetValue);
       }
-      if(e.target.className === "bottom-left"){
+      if(e.target.className === "left"){
         setBottomLeft(targetValue);
       }
     } else {
-      if(e.target.className === "top-left"){
+      if(e.target.className === "top"){
         setTopLeft(0);
       }
-      if(e.target.className === "top-right"){
+      if(e.target.className === "right"){
         setTopRight(0);
       }
-      if(e.target.className === "bottom-right"){
+      if(e.target.className === "bottom"){
         setBottomRight(0);
       }
-      if(e.target.className === "bottom-left"){
+      if(e.target.className === "left"){
         setBottomLeft(0);
       }
     }
@@ -50,43 +50,22 @@ function App() {
   return (
     <div className="screen">
       <div className="border-values">
-        <input
-        type="number"
-        className="top-left"
-        placeholder='top-left'
-        onChange={(e: any) => {handleInputChange(e)}}
-        disabled
-        />
-        <input className='top-left' type="range" value={top} onChange={(e: any) => {handleInputChange(e)}} />
-        <input
-        type="number"
-        className="top-right"
-        placeholder='top-right'
-        onChange={(e: any) => {handleInputChange(e)}}
-        disabled
-        />
-        <input className='top-right' type="range" value={right} onChange={(e: any) => {handleInputChange(e)}}/>
-        <input
-        type="number"
-        className="bottom-right"
-        placeholder='bottom-right'
-        onChange={(e: any) => {handleInputChange(e)}}
-        disabled
-        />
-        <input className='bottom-right' type="range" value={bottom} onChange={(e: any) => {handleInputChange(e)}}/>
-        <input
-        type="number"
-        className="bottom-left"
-        placeholder='bottom-left'
-        disabled
-        />
-        <input className='bottom-left' type="range" value={left} onChange={(e: any) => {handleInputChange(e)}}/>
-      </div>
-      <div className="square">
-        <div className="fancy-board" style={{borderTopLeftRadius: `${top}%`,  borderTopRightRadius: `${right}%`, borderBottomRightRadius: `${bottom}%`, borderBottomLeftRadius: `${left}%`}}>
 
-        </div>
+        <input className='top' type="range" value={top} onChange={(e: any) => {handleInputChange(e)}} />
+
+
+
       </div>
+      <div className="square-container">
+        <input className='left' type="range" value={left} onChange={(e: any) => {handleInputChange(e)}}/>
+        <div className="square">
+          <div className="fancy-board" style={{borderTopLeftRadius: `${top}%`,  borderTopRightRadius: `${right}%`, borderBottomRightRadius: `${bottom}%`, borderBottomLeftRadius: `${left}%`}}>
+
+          </div>
+       </div>
+        <input className='right' type="range" value={right} onChange={(e: any) => {handleInputChange(e)}}/>
+      </div>
+        <input className='bottom' type="range" value={bottom} onChange={(e: any) => {handleInputChange(e)}}/>
       <div className="clip-board">
         <p style={{color: 'white'}}>{`border-radius: ${top}% ${100-top}% ${bottom}% ${100-bottom}% / ${100-left}% ${right}% ${100-right}% ${left}%`}</p>
           <button onClick={copyToClipBoard}>Copy to clipboard</button>
